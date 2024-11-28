2a97e2714910129c9977c3b741f9765f816f67e8
DominikŻuchowicz <dominikzucho@gmail.com>
2024-11-28 19:38:04 +0100

    Edycja maina pod wpisywanie wartości

--- main.cpp
+++ main.cpp
@@ -4,7 +4,18 @@
 using namespace std;
 
 int main() {
-  cout << "Hello, World!" << endl;
+  int iloscLiczb = 0;
+  cout << "Podaj ilosc liczb do posortowania: ";
+  cin >> iloscLiczb;
+  int* tablica = new int[iloscLiczb];
+  cout << "Podaj liczby do posortowania: ";
+  for (int i = 0; i < iloscLiczb; i++) {
+    cin >> tablica[i];
+  }
+  MergeSort tab(iloscLiczb, tablica);
+  tab.mergeSort(tablica, 0, iloscLiczb - 1);
+  tab.print(tablica, iloscLiczb);
+
 
   return 0;
 }
